d454aa351deafccaab85e0fbffa236c805f8e62c
pjechete <prosper@phs-usa.com>
2026-03-12 18:41:02 -0500

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+  return(
+    <footer style={{padding:"40px",borderTop:"1px solid #eee",marginTop:"80px"}}>
+      Prosperity Heritage Advisory
+    </footer>
+  )
+}
