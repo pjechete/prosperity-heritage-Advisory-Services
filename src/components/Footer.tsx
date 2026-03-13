58d1e8523dbf0ff1f5d2ad95c1c222b1448436d5
pjechete <prosper@phs-usa.com>
2026-03-12 19:10:34 -0500

    Delete src/components directory

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-  return(
-    <footer style={{padding:"40px",borderTop:"1px solid #eee",marginTop:"80px"}}>
-      Prosperity Heritage Advisory
-    </footer>
-  )
-}
--- src/components/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar(){
-  return(
-    <header style={{padding:"20px",borderBottom:"1px solid #eee"}}>
-      <strong>Prosperity Heritage Advisory</strong>
-    </header>
-  )
-}
