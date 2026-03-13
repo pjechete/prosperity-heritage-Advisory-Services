36f521e98d98c2e8af839caf483d2dc5e78e611a
pjechete <prosper@phs-usa.com>
2026-03-12 19:08:34 -0500

    Create Navbar.tsx

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <header
+      style={{
+        padding: '20px 32px',
+        borderBottom: '1px solid #e5e7eb',
+        fontWeight: 700,
+      }}
+    >
+      Prosperity Heritage Advisory
+    </header>
+  )
+}
