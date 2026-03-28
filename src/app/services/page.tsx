0984ab4873a2f5f155f95700ea52b34694f823ee
pjechete <prosper@phs-usa.com>
2026-03-28 18:55:21 -0500

    Update page.tsx

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,7 +1,4 @@
 import Link from 'next/link'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-
 export default function ServicesPage() {
   return (
     <main>
