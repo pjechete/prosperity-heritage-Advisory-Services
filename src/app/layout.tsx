e46c18dd0254aa75bdc4e9e6232e9d42d1a47e11
pjechete <prosper@phs-usa.com>
2026-04-06 20:31:03 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,19 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-export const metadata: Metadata = {
-  title: 'Prosperity Heritage Advisory',
-  description: 'Turn Revenue Into Predictable Cash',
-}
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
